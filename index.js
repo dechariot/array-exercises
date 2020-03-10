002f83b8fdc1900e69a982653ef57976694209ee
dechariot <nsprisang@gmail.com>
2020-03-10 11:14:49 +0700

    add

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ let sortAZ = [];
 let sortZA = [];
 let totalNum = [];
 let resultArea = [];
+let RegD = /(\b\S+\b)\s+\b\1\b/
+
 
 //FILTER
 //inventors name start with "a"
@@ -49,17 +51,11 @@ inventorsContainN = inventors.filter((name)=> {
 console.log('The inventors have a name which cotain "n" are ',inventorsContainN)
 
 //inventors name have twice 1st letter in row
-// inventorsTwiceSame = inventors.filter((name)=> {
-//   return name.split(([a-z,A-Z])\1);
-// })
-
-// console.log('The inventors have same twice 1st letter name are ',inventorsTwiceSame)
-
+inventorsTwiceSame = inventors.filter((name)=> {return name.split(RegD)})
+console.log('The inventors have same twice 1st letter name are ',inventorsTwiceSame)
 //Print out an array of the numbers which are odd.
 numberOdd = numbers.filter((num)=> {
-  return num % 2 === 1;
-});
-
+  return num % 2 === 1;});
 console.log(numberOdd);
 
 //Print out an array of the numbers that have two digits.
@@ -99,14 +95,13 @@ toUpperCaseName = inventors.map((name)=> {
 console.log("Print out all the inventors' names in uppercase:",toUpperCaseName);
 
 //Print out initials of all inventors(e.g. A.E., I.N., ...)
-
-// initialsName = inventors.map((name)=> {
-//   for (i = 0; i < name.length; i++){
-//     let letterFirst = name.split(" ");
-//     return letterFirst.charAt(0);
-//   }
-// });
-
+console.log(" Print out initials of all inventors(e.g. A.E., I.N., ...)",inventors.map((name)=>{
+  let nameSplit=name.split(" ");
+  initials =""; 
+  for(let i=0;i<nameSplit.length;i++){
+    initials+=nameSplit[i].charAt(0)
+  };
+}))
 //Print out an array of every number multiplied by 100.
 console.log("Print out an array of every number multiplied by 100", multiplied100 = numbers.map(num=>num*100));
 
@@ -121,9 +116,11 @@ console.log("Sort all the inventors in reverse alphabetical order, Z-A. Do not u
 
 //Sort all the inventors by length of name, shortest name first.
 console.log("Sort all the inventors by length of name, shortest name first.", sortAZ=inventors.sort((a,b)=>{if(a.length > b.length){return 1}; if(a.length < b.length){return -1}; return 0; }));
+console.log(inventors.sort((a,b)=>a.length-b.length))
 
 //Sort all the inventors by length of name, longest name first. Do not use the reverse method.
 console.log("Sort all the inventors by length of name, shortest name first.", sortZA=inventors.sort((a,b)=>{if(a.length > b.length){return -1}; if(a.length < b.length){return 1}; return 0; }));
+console.log(inventors.sort((a,b)=>b.length-a.length))
 
 
 //REDUCE
@@ -142,6 +139,7 @@ console.log("Create a string that has the first name of every inventor.",resultA
 console.log("Create a string that has the first name of every inventor.",resultArea=inventorsFirstName.join(","))
 
 
+//SOME & EVERY
 //Does any inventor have the letter 'y' in their name?
 console.log("Does any inventor have the letter 'y' in their name?",resultArea=inventors.some((name)=>{if(name.includes("y")){return}}));
 
@@ -155,3 +153,16 @@ console.log("Does every inventor have first name that's longer than 4 characters
 // console.log("Find the inventor that has a middle name.",resultArea = inventors.find((name)=>{if(name.findIndex() > 3 ){return name}}));
 
 // inventors.findIndex(function(" "))
+
+
+
+/// TIPS
+
+let number = 3; 
+if (number < 1) {
+  console.log("number < 1")
+} else {
+  console.log("number > 1")
+}
+//short way
+number<1?"number>1":"number>1"
